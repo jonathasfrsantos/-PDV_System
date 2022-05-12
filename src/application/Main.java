1749fd656caafe9d9f469713fe7e5d2883dbb5ea
jonathasfrsantos <jonathasfrsantos@gmail.com>
2022-05-12 20:31:17 -0300

    Third commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,6 +19,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		List<Product> listProductStock = BancoDeDadosFake.buscarProdutos();
+		List<Product> purchasedProducts = new ArrayList<>();
 		
 		double totalValue = 0.0;
 		int count = 0;
@@ -26,6 +27,7 @@ public class Main {
 		double cashValue = 0.0;
 		double changeMoney = 0.0;
 		int numberInstallments = 0;
+		int amountPurchased = 0;
 
 		
 
@@ -53,6 +55,10 @@ public class Main {
 			}
 		} while (enteredCode != 999);
 		
+		System.out.println("_____________________________________ \n");
+		System.out.printf("Subtotal %.2f %n", totalValue);
+		System.out.println("_____________________________________");
+
 		// third stage - payment methods
 		System.out.println();
 		System.out.println("SELECT PAYMENT METHOD : \n  1 - IN CASH \n  2 - DEBIT CARD \n 3 - CREDIT CARD \n");
@@ -70,14 +76,40 @@ public class Main {
 			} 
 
 		} while (chosenPaymentMethod < 1 || chosenPaymentMethod > 3);
-		
-		
 
+		// fourth stage - priting invoice and details of purchase
+		System.out.println("\n ----------------------------");
+		System.out.println(
+				"\n DROGARIA LORE IPSUM \n AVENIDA LORE IPSUM, Nº 100, LOJA 3 \n CENTRO - RIO DE JANEIRO - RJ");
+		System.out.println("\n INVOICE  ");
+		System.out.println("_____________________________________");
+		System.out.println("ITEM ----- CODE ---- DESCRIPTION ---- PRICE R$");
+		for (Product itens : purchasedProducts) {
+			count += 1;
+			System.out.println(count + "----" + itens.getCode() + "----" + itens.getName() + "----" + itens.getPrice());
+		}
 
-		
-		Invoice inv = new Invoice(s1);
-		
-		inv.printInvoice();
+		// fifth stage - total of bought
+
+		System.out.println("____________________________________________________");
+		System.out.println("TOTAL PRODUCTS PURCHASED: " + amountPurchased + " ITENS");
+		if (chosenPaymentMethod == 1) {
+			System.out.printf("MONEY RECEIVED ............... R$ %.2f %n", cashValue);
+			System.out.printf("CHANGE MONEY   ............... R$ %.2f %n", changeMoney);
+		} else if (chosenPaymentMethod == 3) {
+			if (numberInstallments <= 1) {
+				System.out.printf("CREDIT CARD ............... R$ %.2f", totalValue);
+			} else if (numberInstallments > 1) {
+				Double installmentAmount = totalValue / numberInstallments;
+				System.out.printf("CREDIT CARD ............... IN %d X INSTALLMENTS OF %.2f %n", numberInstallments,
+						installmentAmount);
+			}
+		} else if (chosenPaymentMethod == 2) {
+			System.out.printf("DEBIT CARD ............... R$ %.2f %n", totalValue);
+
+		}
+		System.out.println(" ____________________________________________________");
+		System.out.printf("PURCHASED TOTAL ............... R$ %.2f %n", totalValue);
 
 		sc.close();
 	}
